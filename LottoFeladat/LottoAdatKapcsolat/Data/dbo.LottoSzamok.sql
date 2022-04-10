23fef0420c9990e633cc195f7695c763c5d9019e
Koza Gabor <kozagabor79@gmail.com>
2022-04-11 01:12:26 +0200

    C# SkandinávLotto

---
+++ LottoFeladat/LottoAdatKapcsolat/Data/dbo.LottoSzamok.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[LottoSzamok] (
+    [Id]    INT IDENTITY (1, 1) NOT NULL,
+    [Szam1] INT NOT NULL,
+    [Szam2] INT NOT NULL,
+    [Szam3] INT NOT NULL,
+    [Szam4] INT NOT NULL,
+    [Szam5] INT NOT NULL,
+    [Szam6] INT NOT NULL, 
+    [Szam7] INT NOT NULL, 
+    CONSTRAINT [PK_LottoSzamok] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
